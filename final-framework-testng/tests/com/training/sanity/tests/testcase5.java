cfb9d2b473746d669ae91427081ca42489c3ab35
SharvaniK <SharvaniK@9.102.2.110>
2019-08-15 11:23:01 +0530

    mediumlevel test cases

--- final-framework-testng/tests/com/training/sanity/tests/testcase5.java
+++ final-framework-testng/tests/com/training/sanity/tests/testcase5.java
@@ -6,10 +6,10 @@ package com.training.sanity.tests;
 	import java.util.Properties;
 
 	import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+//import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.WebElement;
-	import org.openqa.selenium.support.ui.Select;
+	//import org.openqa.selenium.WebElement;
+	//import org.openqa.selenium.support.ui.Select;
 	import org.testng.annotations.AfterMethod;
 		import org.testng.annotations.BeforeClass;
 		import org.testng.annotations.BeforeMethod;
